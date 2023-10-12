b89f7a53a3e27e39320ea704ee8708223684c616
amusso01 <eandram@hotmail.it>
2023-10-12 11:20:00 +0200

    footer

--- footer.php
+++ footer.php
@@ -11,19 +11,31 @@
 
 ?>
 
+<?php 
+// VAR
+$address = get_field('info_aziendali', 'options');
+?>
 
 
 	<footer class="site-footer color-dark-bg">
-		<div class="site-footer__inner content-block ">
-
+		<div class="content-block">
+			<div class="site-footer__wrapper">
+				<div class="site-footer__item site-footer__item-info">
+					<p class="copyright h6"><?= '&copy; ' . date('Y') . ' <span class="copyright-site-name">' . get_bloginfo('name') .' MINUTERIE METALLICHE SRL</span>'?></p>
+					<div class="address"><?= $address ?></div>
+				</div>
+				<div class="site-footer__item site-footer__item-nav">
+					<?php get_template_part( 'components/navigation/footer-nav' ); ?>
+				</div>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
 
-<!-- MAP MOdal -->
-<?php get_template_part( 'components/modal/map' ); ?>
+<!-- MAP Modal -->
+	<?php get_template_part( 'components/modal/map' ); ?>
 
-<?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 </body>
 </html>
